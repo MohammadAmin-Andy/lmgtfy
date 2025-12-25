cfaed6e74adc5c976495727d283a6168ada62abd
Mamin-Andy <pg.mohammadamin@gmail.com>
2025-12-25 19:26:18 +0330

    add url luncher

--- lib/persian/screens/home_screen.dart
+++ lib/persian/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lmgtfy/persian/model/constants/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
